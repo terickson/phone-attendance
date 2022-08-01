b27ff1ef8cd0346b1d867b77efdac8a9ffca913f
Todd Erickson <todde1981@gmail.com>
2022-08-01 09:00:02 -0500

    building pdf's correctly

--- scripts/barcode-generator/generator.py
+++ scripts/barcode-generator/generator.py
@@ -6,20 +6,28 @@ def main():
     baseImgLocation='/tmp/wrTest'
     pdf = FPDF()
     pdf.set_font("Helvetica")
-    pdf.add_page()
-    for i in range(0, 24):
-        val=str(i+1)
-        use_qr(pdf, baseImgLocation, i, val)
-        #use_code39(pdf, i, val)
-    pdf.output("interleaved2of5.pdf")
+    qcp = 0
+    for idx in range(0, 24):
+        if qcp == 0:
+            pdf.add_page()
+        val=str(idx+1)
+        use_qr(pdf, baseImgLocation, qcp, val)
+        if int(val)%4 == 0:
+            qcp = 0
+        else:
+            qcp+=1
+    pdf.output("barcode.pdf")
 
 def use_qr(pdf, imgLocation, i, val):
+    offset=2
     img = qrcode.make(val)
     imgLocation+=val
     imgLocation+='.png'
     img.save(imgLocation)
-    pdf.image(imgLocation, x=40*(i%5), y=20*(int(i/5)), h=10)
-    pdf.text(x=40*(i%5), y=20*(int(i/5))+14, txt=val)
+    xval = 110*(i%offset)
+    yval = 120*int((i/offset))
+    pdf.text(x=xval+50, y=yval+5, txt=val)
+    pdf.image(imgLocation, x=xval, y=yval+5, h=100)
     os.remove(imgLocation)
 
 def use_code39(pdf, i, val):
